a8c1414a09bd9b0260f0ea68420aae1eb92a2e5e
felipefrdly <104601475+felipefrdly@users.noreply.github.com>
2025-11-14 11:34:47 -0500

    testing again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App! Changes');
+  res.send('Hello, Dockerized Node.js App! More Changes');
 });
 
 app.listen(PORT, () => {
